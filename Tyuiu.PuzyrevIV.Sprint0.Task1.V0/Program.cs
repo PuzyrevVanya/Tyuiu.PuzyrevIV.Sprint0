2ced53f9559599588c8487671cd2f4f56c7e189d
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-09-10 20:17:25 +0500

    Добавил таск1

---
+++ Tyuiu.PuzyrevIV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PuzyrevIV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Иван";
+            int age = 21;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " год!");
+            Console.ReadKey();
+        }
+    }
+}          
