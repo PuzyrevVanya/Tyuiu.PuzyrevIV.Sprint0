1e3266ef60795c05b338a2a36cdb6f0907c0e2e8
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-09-11 11:59:45 +0500

    Task3

---
+++ Tyuiu.PuzyrevIV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PuzyrevIV.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
