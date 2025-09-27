854d95c74ab8978d37f8aeb879b4168360bb3239
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-09-28 01:11:08 +0500

    Task.V7

---
+++ Sprint1/Tyuiu.PuzyrevIV.Sprint1.Task0/Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Sprint1/Tyuiu.PuzyrevIV.Sprint1.Task0/Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
